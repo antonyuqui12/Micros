47405fe3dbd2259e76a4545cc5c03517c9156c09
antonyuqui12 <92069042+antonyuqui12@users.noreply.github.com>
2023-07-02 23:12:01 -0500

    Update semaforo.h

--- SemaforoAntirebote/AntirebotePractica2/src/semaforo.h
+++ SemaforoAntirebote/AntirebotePractica2/src/semaforo.h
@@ -1,35 +1,35 @@
+// Librerias 
+#include <Arduino.h>
 
-#include <Arduino.h>    //libery to arduino
-
-/******** Data declared as structure type ***************/
+// Declaración del tipo de estructura de la información considerando los 3 estados a manejar en el MEF
 typedef enum  {
-  ESTADO_INICIAL,
-  ESTADO_1,
-  ESTADO_2
-}estadoMEF_t;
+  ESTADO_ARRANQUE,
+  ESTADO_PRECAUCION,
+  ESTADO_DETENCION
+  }estadoMEF_t;
 
-/******** Data declared as enumeration type ***************/
+// Declaración del tipo de estructura para las màquinas de estado de los modos del semaforo
 typedef enum {
-  NORMAL_MODE,
-  DISCONNECTED_MODE,
-  ALARM_MODE,
+  NORMAL,
+  DESCONECTADO,
+  ALARMA,
 } fsmTrafficLightMode_t;
 
-/*********** declaration of variables as external *********/
+// Variables externas
 extern fsmTrafficLightMode_t fsmTrafficMode;
-extern int16_t baseTime;     // Base time for the program when it starts
+extern int16_t baseTime;     // Tiempo de base de operación
 
 estadoMEF_t estadoActual;
 
-/*********** function to initialize MEF of the traffic light*/
+// Funcion de arranque de la maquina de estados de semaforo
 void fsmTrafficLightInit(int16_t LED_GREEN, int16_t LED_YELLOW, int16_t LED_RED) {
-  fsmTrafficMode = NORMAL_MODE;
+  fsmTrafficMode = NORMAL;
   digitalWrite(LED_GREEN, LOW);
   digitalWrite(LED_YELLOW, LOW);
   digitalWrite(LED_RED, LOW);  
 }
 
-/********* function to set mode the traffic light **********/
+// Funcion de presentación de semaforo al arranque
 void setTrafficLightMode(fsmTrafficLightMode_t mode,int16_t LED_GREEN, int16_t LED_YELLOW, int16_t LED_RED) {
   fsmTrafficMode = mode;
   digitalWrite(LED_GREEN, LOW);
@@ -37,70 +37,70 @@ void setTrafficLightMode(fsmTrafficLightMode_t mode,int16_t LED_GREEN, int16_t L
   digitalWrite(LED_RED, LOW);
 }
 
-/********* function to change mode the traffic light **********/
+// Funcion de cambio de modos de semaforo basandose en las maquinas de estado del semaforo
 void changeTrafficLightMode(int16_t LED_GREEN, int16_t LED_YELLOW, int16_t LED_RED) {
   switch (fsmTrafficMode) {
-    case NORMAL_MODE:
-      setTrafficLightMode(DISCONNECTED_MODE, LED_GREEN, LED_YELLOW, LED_RED);
+    case NORMAL:
+      setTrafficLightMode(DESCONECTADO, LED_GREEN, LED_YELLOW, LED_RED);
       break;
-    case DISCONNECTED_MODE:
-      setTrafficLightMode(ALARM_MODE, LED_GREEN, LED_YELLOW, LED_RED);
+    case DESCONECTADO:
+      setTrafficLightMode(ALARMA, LED_GREEN, LED_YELLOW, LED_RED);
       break;
-    case ALARM_MODE:
-      setTrafficLightMode(NORMAL_MODE, LED_GREEN, LED_YELLOW, LED_RED);
+    case ALARMA:
+      setTrafficLightMode(NORMAL, LED_GREEN, LED_YELLOW, LED_RED);
       break;
   }
 }
 
-/****** function to initialize MEF of the traffic light in normal mode*/
+// Funcion de arranque del MEF considerando el estado normal 
 void InicializarMEF() {
-  estadoActual = ESTADO_INICIAL;
+  estadoActual = ESTADO_ARRANQUE;
 }
 
 /****** Function to update state of FEM traffic light in normal mode ******/
 void ActualizarMEF(int16_t baseTime,int16_t LED_GREEN, int16_t LED_YELLOW, int16_t LED_RED) {
   switch (estadoActual) {
-    case ESTADO_INICIAL:
-      if (nbDelay(baseTime*4)) {    //time that will last in ESTADO_2 before returning to ESTADO_INICIAL
+    case ESTADO_ARRANQUE:
+      if (nbDelay(baseTime*4)) {              //tiempo que durará en ESTADO_ARRANQUE antes de pasar a ESTADO_PRECAUCION
         digitalWrite(LED_GREEN, HIGH);
         digitalWrite(LED_YELLOW, LOW);
         digitalWrite(LED_RED, LOW);
-        estadoActual = ESTADO_1;
+        estadoActual = ESTADO_PRECAUCION;
       }
       break;
-    case ESTADO_1:
-      if (nbDelay(baseTime*6)) {  //time that will remain in ESTADO_INICIAL before going to ESTADO_1
-        digitalWrite(LED_GREEN, HIGH);
+    case ESTADO_PRECAUCION:
+      if (nbDelay(baseTime*6)) {            //tiempo que durará en ESTADO_PRECAUCION antes de pasar a ESTADO_DETENCION
+        digitalWrite(LED_GREEN, LOW);
         digitalWrite(LED_YELLOW, HIGH);
         digitalWrite(LED_RED, LOW);
-        estadoActual = ESTADO_2;
+        estadoActual = ESTADO_DETENCION;
       }
       break;
-    case ESTADO_2:
-      if (nbDelay(baseTime)) {    //time that will remain in ESTADO_1 before going to ESTADO_2
+    case ESTADO_DETENCION:
+      if (nbDelay(baseTime)) {              //tiempo que durará en ESTADO_PRECAUCION antes de pasar a ESTADO_ARRANQUE
         digitalWrite(LED_GREEN, LOW);
         digitalWrite(LED_YELLOW, LOW);
         digitalWrite(LED_RED, HIGH);
-        estadoActual = ESTADO_INICIAL;
+        estadoActual = ESTADO_ARRANQUE;
       }
       break;
   }
 }
 
-/************ function to update MEF of traffic light ************/
+// Función de actualziación del estado del MEF del semaforo
 void fsmTrafficLightUpdate(int16_t LED_GREEN, int16_t LED_YELLOW, int16_t LED_RED) {
   switch (fsmTrafficMode) {
-    case NORMAL_MODE:
+    case NORMAL:
     ActualizarMEF(baseTime, LED_GREEN, LED_YELLOW, LED_RED);
     break;
-    case DISCONNECTED_MODE:
+    case DESCONECTADO:
     if (nbDelay(baseTime)){
-        digitalWrite(LED_YELLOW, !digitalRead(LED_YELLOW)); //flashes yellow led with a delay time of baseTime
+        digitalWrite(LED_YELLOW, !digitalRead(LED_YELLOW)); // Parpadear el led naranja en funcion de la base de tiempo estipulada
     }
     break;
-    case ALARM_MODE:
+    case ALARMA:
     if (nbDelay(baseTime)){
-        digitalWrite(LED_RED, !digitalRead(LED_RED)); //flashes red led with a delay time of baseTime
+        digitalWrite(LED_RED, !digitalRead(LED_RED)); //Parpadear el led rojo con un tiempo en función de la base de tiempo
     }
     break;
   }
