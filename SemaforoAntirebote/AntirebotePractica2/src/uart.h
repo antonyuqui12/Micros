8214ec49fd1f0e3954f80019c36795c65e87fea2
antonyuqui12 <92069042+antonyuqui12@users.noreply.github.com>
2023-07-02 23:18:58 -0500

    Update uart.h

--- SemaforoAntirebote/AntirebotePractica2/src/uart.h
+++ SemaforoAntirebote/AntirebotePractica2/src/uart.h
@@ -1,14 +1,15 @@
-/****************************Declare libraries***************************/
+// Librerias
 #include "Arduino.h"
-/***************************Function to print key state**************/
+
+// Funcion de impresión de estado
 void myPrint(dbn_t* data){
   if (data->estado == 1){
     Serial.print("Tecla: ");
-    Serial.print(data->tecla-1); //Print in the UART communication of the state of the Key
+    Serial.print(data->tecla-1);    //Impresión del estado correspondiente de la clave
     Serial.print("\t Estado: Presionado\n");
   } else if (data->estado == 0){
     Serial.print("Tecla: ");
-    Serial.print(data->tecla-1); //Print in the UART communication of the state of the Key
+    Serial.print(data->tecla-1);    //Impresión del estado correspondiente de la clave
     Serial.print("\t Estado: Liberado\n");
   }
   
