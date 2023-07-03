35fe399e726a5892bfc43446170509dfc901cc89
antonyuqui12 <92069042+antonyuqui12@users.noreply.github.com>
2023-07-02 22:50:24 -0500

    Update semaforo.c

--- SemaforoAntirebote/AntirebotePractica2/src/semaforo.c
+++ SemaforoAntirebote/AntirebotePractica2/src/semaforo.c
@@ -1,11 +1,17 @@
 #include "semaforo.h"
 
-/****************** Prototype functions of semaforo.h ******************/
+// Funciones correspondientes a semaforo.h 
 int16_t nbDelay(int16_t t_delay);
+
+// Máquina destado de inicio del semaforo con la actualziación de estado del modo de operación
 void fsmTrafficLightInit(int16_t LED_GREEN, int16_t LED_YELLOW, int16_t LED_RED);
 void setTrafficLightMode(fsmTrafficLightMode_t mode,int16_t LED_GREEN, int16_t LED_YELLOW, int16_t LED_RED);
+
+// Máquina de estados de actualziación de semaforo con actualización del modo de operación
 void fsmTrafficLightUpdate(int16_t LED_GREEN, int16_t LED_YELLOW, int16_t LED_RED);
 void changeTrafficLightMode(int16_t LED_GREEN, int16_t LED_YELLOW, int16_t LED_RED);
+
+//Actualziación del MEF
 void ActualizarMEF(int16_t baseTime,int16_t LED_GREEN, int16_t LED_YELLOW, int16_t LED_RED);
 void InicializarMEF();
 
