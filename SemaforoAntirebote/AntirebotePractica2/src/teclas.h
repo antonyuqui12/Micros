2dddc3e744553e4716c8391ce0f6c5919b4f90ae
antonyuqui12 <92069042+antonyuqui12@users.noreply.github.com>
2023-07-03 22:16:44 -0500

    Update teclas.h

--- SemaforoAntirebote/AntirebotePractica2/src/teclas.h
+++ SemaforoAntirebote/AntirebotePractica2/src/teclas.h
@@ -1,30 +1,36 @@
-/****************************Declare libraries ********************************/
+// Librerias
 #include <Arduino.h>
 #include <stdio.h>
-/**********************Declare a function enumerated*************************/
+
+
+// Declarar la enumeración de la función
 typedef enum {
   BUTTON_UP,
   BUTTON_DOWN,
   BUTTON_FALLING,
   BUTTON_RISING,
 } fsmButtonState_t;
-/*****************DEclare a function struct**********************************/
+
+
+// Declarar la estructura de la función
 typedef struct {
   int16_t tecla;
   int16_t delay;
   fsmButtonState_t estado;
 } dbn_t;
-/*************************Declaration of external variables******************/
+
+// Declaración de las variables externas
 extern dbn_t dbnSW1;
 extern dbn_t dbnSW2;
-extern int16_t baseTime;                  // Base time for the program when it starts
+extern int16_t baseTime;                  // Tiempo de base para el arranque
 
-/******************************Definition of the print function*************/
+// Definición de la función de impresión de información
 typedef void (*PrintFunction)(dbn_t* data);
-/********************Definition of the light mode change function************/
+
+// Definición de la función de cambio de modo de iluminación
 void changeTrafficLightMode(int16_t LED_GREEN, int16_t LED_YELLOW, int16_t LED_RED);
 
-/******************Non-blocking delay Function**************/
+// Función de retardo antibloqueo
 int16_t nbDelay(int16_t t_delay) {
   int16_t t_base = 1;
   static int16_t count = 0;
@@ -42,21 +48,22 @@ int16_t nbDelay(int16_t t_delay) {
 
 
 
-/************************* Function of Button Init FSM *******************/
+// Inicion de la máquina de estados del inicio del boton 
 void fsmButtonInit(dbn_t* data) {
   data->estado = BUTTON_UP;
 }
 
-/******************************Function of Button Errorf ***************************/
+// Funcion de definición de error del botón
 void fsmButtonError(dbn_t* data) {
   data->estado = BUTTON_UP;
 }
 
 
-/*********************Function of the button pressed********************/
+// Función de guardado del estado del botón
 void buttonPressed(dbn_t* data, int16_t SW1, int16_t SW2,int16_t LED_GREEN, int16_t LED_YELLOW, int16_t LED_RED, PrintFunction printFunc) {
   if (data->tecla == SW1) {
-    // Change the time base of normal mode
+    
+// Cambio del tiempo base al estado normal
     baseTime = baseTime / 2;
 
     if (baseTime < 250) {
@@ -64,48 +71,50 @@ void buttonPressed(dbn_t* data, int16_t SW1, int16_t SW2,int16_t LED_GREEN, int1
     }
     printFunc(data);
   } else if (data->tecla == SW2) {
-    // Change the mode of operation of the traffic light
+    
+// Cambio del modo de operación del semáforo
     changeTrafficLightMode(LED_GREEN,LED_YELLOW,LED_RED);
     printFunc(data);
   }
 }
 
 
-/************************************* Function of Released Button***************************/
-
+//Función de actualización del estado del botón
 void buttonReleased(dbn_t* data, int16_t SW1, int16_t SW2, PrintFunction printFunc) {
     printFunc(data);
 }
-/****************************Update Button Function***************************************/
+
+// Ingresar actualización del estado nuevo del botón
 void fsmButtonUpdate(dbn_t* data,int16_t SW1, int16_t SW2,int16_t LED_GREEN, int16_t LED_YELLOW, int16_t LED_RED, PrintFunction printFunc) {
   switch (data->estado) {
     case BUTTON_UP:
       if (!digitalRead(data->tecla)) {
-        data->estado = BUTTON_FALLING; //Si se detecta un cambio en el pulsante se manda al estado Falling
+        data->estado = BUTTON_FALLING; //Si el estado del Swicth cambia se manda a falling
       }
       break;
     case BUTTON_DOWN:
       if (digitalRead(data->tecla)) {
-        data->estado = BUTTON_RISING; // Si se detecta un cambio en el pulsante 
+        data->estado = BUTTON_RISING; //El cambio en el Swicth manda a rising 
       }
       break;
     case BUTTON_FALLING:
       if (nbDelay(data->delay)) {
-        if (!digitalRead(data->tecla)) { // Si pasado el tiempo del retardo la tecla cambia de estado mandar a down 
+        if (!digitalRead(data->tecla)) { // Pasado el tiempo de la interrupción el estado del swicth pasa a down 
           data->estado = BUTTON_DOWN;
           buttonPressed(data, SW1,SW2,LED_GREEN,LED_YELLOW,LED_RED, printFunc);
         } else {
-          data->estado = BUTTON_UP;// Si pasado el tiempo del retardo la tecla cambia de estado mandar a up
+          data->estado = BUTTON_UP;// Pasado el tiempo de la interrupción el estado del swicth pasa a up
         }
       }
       break;
+
     case BUTTON_RISING:
       if (nbDelay(data->delay)) {
-        if (digitalRead(data->tecla)) { // Si pasado el tiempo del retardo la tecla cambia de estado mandar a up
+        if (digitalRead(data->tecla)) { 
           data->estado = BUTTON_UP;
           buttonReleased(data, SW1, SW2, printFunc);
         } else {
-          data->estado = BUTTON_DOWN; // Si pasado el tiempo del retardo la tecla cambia de estado mandar a down
+          data->estado = BUTTON_DOWN; 
         }
       }
       break;
